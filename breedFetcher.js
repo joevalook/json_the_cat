112d3d0bdee823e6b5a7acff2992959ccdd5c15f
Joe Valookaran <joevalookaran@hotmail.com>
2022-01-20 19:35:29 +0000

    made the mocha and chai tests, as well as refactored code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,18 @@
 const request = require('request');
-let breed = process.argv[2];
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
-  if (error !== null) {
-    console.log('error: the reuqest has failed! something must be wrong on our end oops '); // Print the error if one occurred
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("That's not a valid breed!");
-  } else {
-    console.log(data[0]["description"]);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (error !== null) {
+      return callback(('error: the reuqest has failed! something must be wrong on our end oops '), null); // Print the error if one occurred
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      return callback(("That's not a valid breed!"), null);
+    } else {
+      return callback(null,(data[0]["description"]));
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
